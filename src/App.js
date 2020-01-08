5d8c66a178f1f6707c45456d2ff4e729981f622f
Palm <nipalm@f45c89acd433.ant.amazon.com>
2020-01-08 17:14:59 +0100

    Minor cleanup

--- src/App.js
+++ src/App.js
@@ -31,12 +31,10 @@ const App = () => {
 
   useEffect(() => {
     if (otherStreams.length > 0) {
-      console.log("otherStreams changed");
       console.log("Num other streams: ", otherStreams.length);
       console.log("Stream to add: ", otherStreams[0]);
 
       let other = document.querySelector("#other_stream");
-      console.log("Stream will be added as source to: ", other);
       other.srcObject = otherStreams[0];
     }
   }, [otherStreams]);
@@ -44,8 +42,6 @@ const App = () => {
   if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
     videoRef.current.srcObject = mediaStream;
   }
-
-  console.log("App re-rendered. Other streams: ", otherStreams);
 
   const handleCanPlay = () => {
     videoRef.current.play();
